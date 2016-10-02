2aa8470152ccde6bbc3762b55dc7d720b20ca954
siddnhk <siddnhk@gmail.com>
2016-10-02 17:28:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var img= getElementById('madi');
+var marginleft=0;
+function moveright(){
+
+       marginleft= marginleft+10;
+       img.style.marginleft= marginleft+'px';
+}
+
+img.onclick = function(){
+       var interval= setInterval(moveright,100);
+};
