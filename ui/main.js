0e007981ab82fef9fc6f2d04740036e4b11f1009
siddnhk <siddnhk@gmail.com>
2016-10-13 12:46:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,31 @@
-console.log('Loaded!');
-var img= document.getElementById('madi');
-var marginLeft=0;
-function moveright(){
 
-       marginLeft= marginLeft+10;
-       img.style.marginLeft= marginLeft+'px';
-}
+var submit = document.getElementById('submit_btn');
 
-img.onclick = function(){
-       var interval= setInterval(moveright,100);
-};
+submit.onclick = function(){
+   
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i< names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+     
+    
+    };
+   varnameInput = document.getElementById('name');
+   var name = nameInput.value;
+   request.open('GET', 'http://siddnhk.imad.hasura-app.io/submit-name?name=' + name, true);
+   request.send(null);
+   
+    
+} ;
