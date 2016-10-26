2d67e42b9ca16a570ea9c0d13446dbd4a2a369a9
siddnhk <siddnhk@gmail.com>
2016-10-27 02:06:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+ host: 'db.imad.hasura-app.io',
+ user: 'siddnhk',
+ password: process.env.DB_PASSWORD,
+ port: '5432',
+ database: 'siddnhk',
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -62,6 +71,24 @@ function createTemplate(data){
         `;
      return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  
+  pool.query('SELECT * FROM test', function(err,result){
+         if(err){
+       res.status(500).send(err.toString());
+   }   else{
+       
+       res.send(JSON.stringify(result));
+   }
+  
+  });
+
+});
+
+
+
  
 var names = []; 
 app.get('/submit-name', function (req, res) {
