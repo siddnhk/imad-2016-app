70ad6e95129785ad9671f5e05ac0e461eec1560b
siddnhk <siddnhk@gmail.com>
2016-10-27 02:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createTemplate(data){
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
   
-  pool.query('SELECT * FROM articles', function(err,result){
+  pool.query('SELECT date FROM articles', function(err,result){
          if(err){
        res.status(500).send(err.toString());
    }   else{
