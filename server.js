38e523440b125c18cf6d4ef522b1fbb9a92e3bdb
siddnhk <siddnhk@gmail.com>
2016-10-27 16:54:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,9 @@ app.get('/articles/:articleName', function (req, res) {
         } else{
              
             var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
+           //res.send(createTemplate(articleData));
+        
+             res.send(JSON.stringify(articleData));
         }
     }
 
