55325b00e4f6653fe4bdb19c6f831514999925ee
siddnhk <siddnhk@gmail.com>
2016-10-27 15:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,21 +102,20 @@ app.get('/submit-name', function (req, res) {
 app.get('/articles/:articleName', function (req, res) {
   
   
-//  pool.query("SELECT * FROM articles WHERE title ='", + req.params.articleName +"'",function(err,result){
-   // if(err){
-     //  res.status(500).send(err.toString());
-//    } else{
-  //      if (result.rows.length === 0){
-    //       res.status(404).send('Article not found');
-      //  } else{
-        //    var articlesData = results.rows[0];
-//            res.send(createTemplate(articleData));
-  //      }
-//    }
+  pool.query("SELECT * FROM articles WHERE title ='", + req.params.articleName +"'",function(err,result){
+    if(err){
+       res.status(500).send(err.toString());
+    } else{
+        if (result.rows.length === 0){
+           res.status(404).send('Article not found');
+        } else{
+            var articlesData = results.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
 
-//  }); 
-  console.log(req.params.articleName);
-  res.send(createTemplate(sidOne));
+  }); 
+
   
 });
 
