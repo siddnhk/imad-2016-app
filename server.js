ad07c5cab65515e403fa84285e5c7a36cd7e88d8
siddnhk <siddnhk@gmail.com>
2016-10-27 15:08:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,8 @@ app.get('/submit-name', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
   
-  pool.query("SELECT * FROM articles WHERE title ='", +req.params.articleName+"'",function(err,result){
+  
+//  pool.query("SELECT * FROM articles WHERE title ='", + req.params.articleName +"'",function(err,result){
    // if(err){
      //  res.status(500).send(err.toString());
 //    } else{
@@ -112,15 +113,9 @@ app.get('/articles/:articleName', function (req, res) {
 //            res.send(createTemplate(articleData));
   //      }
 //    }
-   if(err){
-       res.status(500).send(err.toString());
-   }   else{
-       
-       res.send(JSON.stringify(result));
-   }
 
-  }); 
-  
+//  }); 
+  console.log(req.params.articleName);
   
 });
 
