520633f728a7511f8011e40fb6179559955707d0
siddnhk <siddnhk@gmail.com>
2016-10-28 12:35:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
   
   pool.query('SELECT * FROM articles', function(err,result){
-         if(err){
+    if(err){
        res.status(500).send(err.toString());
-   }   else{
+    } else{
        
        res.send(JSON.stringify(result));
    }
