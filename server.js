a14439c53885b2e65586949ecb053a2f42345514
siddnhk <siddnhk@gmail.com>
2016-11-01 13:27:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/post-comments', function (req, res) {
 app.get('/articles/:articleName', function (req, res) {
   
   
-  pool.query("SELECT * FROM articles WHERE title ='" + req.params.articleName +"'",function(err,result){
+  pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName],function(err,result){
     if(err){
        res.status(500).send(err.toString());
     } else{
