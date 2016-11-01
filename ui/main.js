8216da12a81dde0c7e21f1e5e83d449628074ad4
siddnhk <siddnhk@gmail.com>
2016-11-01 20:23:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,14 +9,13 @@ post.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var id=document.getElementById('articleId');
-                var index=id.value;
+                
                 var names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
                 for(var i=0; i< names.length; i++){
                     
-                    list += '<li>' + names[i]+''+id+'</li>';
+                    list += '<li>' + names[i]+'</li>';
                 }
                 var ul = document.getElementById('commentlist');
                 ul.innerHTML = list;
