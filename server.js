f4fc3187334225b6e24e354436067024cf48f540
siddnhk <siddnhk@gmail.com>
2016-11-01 14:19:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/post-comments', function (req, res) {
     
  
   var comment = req.query.comment;
-  var articleId = req.query.index;
+  //var articleId = req.query.index;
   comments.push(comment);
   res.send(JSON.stringify(comments));
   
