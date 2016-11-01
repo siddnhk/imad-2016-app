d439b93f87d7183acb568f7517940962989f5298
siddnhk <siddnhk@gmail.com>
2016-11-01 13:03:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,6 +116,7 @@ app.get('/articles/:articleName', function (req, res) {
     }
 
   }); 
+  }); 
 
   
 app.get('/ui/main.js', function (req, res) {
@@ -141,31 +142,4 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-
-
 
