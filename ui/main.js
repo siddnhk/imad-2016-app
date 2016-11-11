44cd640805ca3aeb6ac887a2b95e2f7f23b81303
siddnhk <siddnhk@gmail.com>
2016-11-12 02:40:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,8 @@ function loadLoginForm () {
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Sign Up" />
-       `;
+        
+        `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
     // Submit username/password to login
