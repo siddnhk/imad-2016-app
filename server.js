afc005c1574129e5e793a8651b0a1118bcf8b10b
siddnhk <siddnhk@gmail.com>
2016-11-20 02:49:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/get-articles', function (req, res) {
